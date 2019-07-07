9a9df9563c993ea8035388a4b5b67d2c10e1849d
0TH0N <jbator777@gmail.com>
2019-07-07 11:13:18 +0300

    Some changes

--- config/DBConnection.php
+++ config/DBConnection.php
@@ -11,7 +11,7 @@ class DBConnection
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES => false,
         ];
-        $pdo = new \PDO('sqlite:db.sqlite', 'test', 'test', $opt);
+        $pdo = new \PDO('mysql:host=localhost;dbname=test', 'test', 'test', $opt);
         return $pdo;
     }
 }
